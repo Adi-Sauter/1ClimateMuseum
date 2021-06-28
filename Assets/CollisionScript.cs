44cae673489d719c56c312e7c47324f721d53e40
Adi-Sauter <adriansauter07.As@gmail.com>
2021-06-28 18:01:17 +0200

    OOOON

--- Assets/CollisionScript.cs
+++ Assets/CollisionScript.cs
@@ -8,14 +8,14 @@ public class CollisionScript : MonoBehaviour
     public GameObject targetinMus;
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision with " + other.gameObject.name);
+        //Debug.Log("Collision with " + other.gameObject.name);
         if(other.gameObject.CompareTag("toSimulator"))
         {
-            Debug.Log("if-case: Collision with " + other.gameObject.name);
+            //Debug.Log("if-case: Collision with " + other.gameObject.name);
             this.transform.localPosition = new Vector3(targetinSim.transform.position.x, this.transform.position.y, targetinSim.transform.position.z);
         } else if(other.gameObject.CompareTag("toMuseum"))
         {
-            Debug.Log("elseif-case: Collision with " + other.gameObject.name);
+            //Debug.Log("elseif-case: Collision with " + other.gameObject.name);
             this.transform.localPosition = new Vector3(targetinMus.transform.position.x, this.transform.position.y, targetinMus.transform.position.z);
         }
     }

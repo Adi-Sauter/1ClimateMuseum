cf6ddbcb167a8ac983770ae62f9f6688f99c8310
Adi-Sauter <adriansauter07.As@gmail.com>
2021-06-30 17:14:27 +0200

    Update_At_Tonis

--- Assets/Scripts/CollisionScript.cs
+++ Assets/Scripts/CollisionScript.cs
@@ -9,6 +9,8 @@ public class CollisionScript : MonoBehaviour
     public GameObject targetinMus;
 
     public GameObject FadeScreen;
+
+    public GameObject BlackScreenCanvas;
     void OnTriggerEnter(Collider other)
     {
         StartCoroutine(Teleportation(other));
@@ -33,20 +35,27 @@ public class CollisionScript : MonoBehaviour
         FadeScreen.GetComponent<Animation>().Play("BlackFade");
     }
 
+
+
     IEnumerator Teleportation(Collider other) 
     {
         if(other.gameObject.CompareTag("toSimulator"))
         {
+            BlackScreenCanvas.SetActive(true);
             //Debug.Log("if-case: Collision with " + other.gameObject.name);
             FadingOut();
             yield return new WaitForSeconds(0.5f);
             this.transform.localPosition = new Vector3(targetinSim.transform.position.x, this.transform.position.y, targetinSim.transform.position.z);
+            BlackScreenCanvas.SetActive(false);
+
         } else if(other.gameObject.CompareTag("toMuseum"))
         {
+            BlackScreenCanvas.SetActive(true);
             //Debug.Log("elseif-case: Collision with " + other.gameObject.name);
             FadingOut();
             yield return new WaitForSeconds(0.5f);
             this.transform.localPosition = new Vector3(targetinMus.transform.position.x, this.transform.position.y, targetinMus.transform.position.z);
+            BlackScreenCanvas.SetActive(false);
         }
         //Debug.Log("Wating");
         //yield return new WaitForSeconds(2);
